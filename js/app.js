02fc6f9c53836bd8c095731249cd9b12900917ad
MARIO ACEVEDO <JUN_ACEVEDO415@YAHOO.COM>
2021-12-18 15:29:11 -0800

    solved problem one

--- js/app.js
+++ js/app.js
@@ -9,11 +9,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+let sum = a + b;
+let message = `The sum of ${4} and ${7} is ${sum}.`;
+return [sum,message];
 }
+let sumReturn = sum(4,7);
+console.log(sumReturn);
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,12 +30,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(5, 9) { //eslint-disable-line
+let sum = 3 + 5;
+let message = `The product of ${5} and ${9} is ${sum}.`
+return [sum, message];
 }
+let sumReturn = sum(5,9)
+console.log(sumReturn);
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
